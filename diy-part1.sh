7727e5215dd4eef2cc8ded9950b3224a973de678
jtzgc <63477893+jtzgc@users.noreply.github.com>
2025-05-25 02:13:42 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+find /home/op/openwrt -name "*passwall*" | xargs rm -rf
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/jtzgc/openwrt-passwall' >>feeds.conf.default
